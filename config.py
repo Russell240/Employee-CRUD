ee810551b7ecb8286309c00858d850c125fee35e
Russell Flaherty <flahertypro1@outlook.com>
2021-11-29 16:22:40 +0000

    start of project

---
+++ config.py
@@ -0,0 +1,30 @@
+SECRET_KEY = '1234$%'
+SQLALCHEMY_DATABASE_URI =  'mysql://flahe:''@localhost/flaskproject'
+
+class Config(object):
+    """
+    Common configurations
+    """
+
+    # Put any configurations here that are common across all environments
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+    DEBUG = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
+
