36fdf018970c1f4dccc57e46d22287e35e570111
Russell Flaherty <flahertypro1@outlook.com>
2021-12-01 16:31:30 +0000

    changes to env file properties

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,9 @@
 from flask import Flask, config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-
 from flask_migrate import Migrate
+
+
 
 
 # local imports
@@ -26,17 +27,20 @@ def create_app():
     app = Flask(__name__, instance_relative_config=True)
     #app.config.from_object(Config)
     app.config.from_object(["development"])
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://flahe:password''@localhost/Employee"
-   
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/employees'
     
-    db.init_app(app)
      # temporary route
     login_manager.init_app(app) 
     login_manager.login_message = "You must be logged in to access this page."
     login_manager.login_view = "auth.login"
     migrate= Migrate(app, db)
-    from app import models
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.app_context().push()
     
+    
+    from app import models
     return app
     
     
