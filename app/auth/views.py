c6c180f43716e23c8b42cb6966602533025f7f0f
Russell Flaherty <flahertypro1@outlook.com>
2022-01-02 10:24:36 +0000

    added email validation package

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,10 +4,11 @@ from flask import flash, redirect, render_template, url_for
 from flask_login import login_required, login_user, logout_user
 
 from . import auth
-from forms import LoginForm, RegistrationForm
+from .forms import LoginForm, RegistrationForm
 from .. import db
 from ..models import Employee
 
+# http://127.0.0.1/register
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     """
